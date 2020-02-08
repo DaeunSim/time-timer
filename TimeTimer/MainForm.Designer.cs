e907204dadf6f73216a60c2ce6c50ddd29d058b6
DaeunSim <daeunnsim@gmail.com>
2020-02-08 15:18:48 +0900

    Add TimeTimer project

---
+++ TimeTimer/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TimeTimer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.ResetBtn = new System.Windows.Forms.Button();
+            this.MinBtn = new System.Windows.Forms.NumericUpDown();
+            this.OkBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MinBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.BackColor = System.Drawing.Color.White;
+            this.StartBtn.Enabled = false;
+            this.StartBtn.FlatAppearance.BorderSize = 0;
+            this.StartBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartBtn.Location = new System.Drawing.Point(85, 11);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(82, 36);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "START";
+            this.StartBtn.UseVisualStyleBackColor = false;
+            this.StartBtn.Visible = false;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.BackColor = System.Drawing.Color.White;
+            this.CloseBtn.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.CloseBtn.Location = new System.Drawing.Point(318, 11);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(32, 26);
+            this.CloseBtn.TabIndex = 2;
+            this.CloseBtn.Text = "X";
+            this.CloseBtn.UseVisualStyleBackColor = false;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // ResetBtn
+            // 
+            this.ResetBtn.BackColor = System.Drawing.Color.White;
+            this.ResetBtn.FlatAppearance.BorderSize = 0;
+            this.ResetBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetBtn.Location = new System.Drawing.Point(175, 11);
+            this.ResetBtn.Name = "ResetBtn";
+            this.ResetBtn.Size = new System.Drawing.Size(82, 36);
+            this.ResetBtn.TabIndex = 3;
+            this.ResetBtn.Text = "RESET";
+            this.ResetBtn.UseVisualStyleBackColor = false;
+            this.ResetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
+            // MinBtn
+            // 
+            this.MinBtn.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MinBtn.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinBtn.Location = new System.Drawing.Point(12, 12);
+            this.MinBtn.Name = "MinBtn";
+            this.MinBtn.Size = new System.Drawing.Size(67, 35);
+            this.MinBtn.TabIndex = 0;
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.BackColor = System.Drawing.Color.White;
+            this.OkBtn.FlatAppearance.BorderSize = 0;
+            this.OkBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OkBtn.Location = new System.Drawing.Point(85, 12);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(82, 36);
+            this.OkBtn.TabIndex = 4;
+            this.OkBtn.Text = "OK";
+            this.OkBtn.UseVisualStyleBackColor = false;
+            this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(364, 311);
+            this.ControlBox = false;
+            this.Controls.Add(this.OkBtn);
+            this.Controls.Add(this.ResetBtn);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.MinBtn);
+            this.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "TimeTimer";
+            ((System.ComponentModel.ISupportInitialize)(this.MinBtn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button ResetBtn;
+        private System.Windows.Forms.NumericUpDown MinBtn;
+        private System.Windows.Forms.Button OkBtn;
+    }
+}
+
---
+++ TimeTimer/MainForm.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TimeTimer
+{
+    public partial class MainForm : Form
+    {
+        private Graphics graphics;
+        private Timer timer;
+        private int min = 0;
+        private bool isStarted = false;
+        private bool canDraw = true;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            graphics = this.CreateGraphics();
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            if (timer == null)
+            {
+                timer = new Timer();
+            }
+
+            timer.Tick += OnPaintUpdate;
+            timer.Interval = 60000;
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams handleParam = base.CreateParams;
+                handleParam.ExStyle |= 0x02000000;
+                return handleParam;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (!canDraw)
+            {
+                return;
+            }
+
+            DrawCircle();
+            DrawPie();
+            DrawDot();
+            DrawMinNumber();
+            DrawCheck();
+
+            canDraw = false;
+        }
+
+        private void DrawCircle()
+        {
+            graphics.FillEllipse(Brushes.Red, 80, 80, 200, 200);
+        }
+
+        private void DrawDot()
+        {
+            graphics.FillEllipse(Brushes.Black, 178, 178, 5, 5);
+        }
+
+        private void DrawPie()
+        {
+            graphics.FillPie(Brushes.White, 79, 79, 203, 203, -90, (float)min * 6);
+        }
+
+        private void DrawMinNumber()
+        {
+            int minStr = 60;
+            String str;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                double radius = (double)((double)((double)i / 12.0 * 360.0 - 90) / 180) * Math.PI;
+                double x = 172 + (180 / 2 - 5) * Math.Cos(radius);
+                double y = 172 + (180 / 2 - 5) * Math.Sin(radius);
+
+                minStr -= 5;
+
+                if (minStr == 0)
+                {
+                    str = minStr.ToString();
+                    minStr = 60;
+                }
+                else
+                {
+                    str = minStr.ToString();
+                }
+
+                graphics.DrawString(str, new Font("Arial", 11f), Brushes.Black, (float)x, (float)y);
+            }
+        }
+
+        private void DrawCheck()
+        {
+            if (min < 60)
+            {
+                return;
+            }
+
+            min = 0;
+
+            canDraw = false;
+
+            if (timer != null)
+            {
+                timer.Stop();
+            }
+
+            if (CMessageBox.Show(this, "Time is over") == DialogResult.OK)
+            {
+                isStarted = false;
+                StartBtn.Text = "START";
+
+                canDraw = true;
+
+                Invalidate();
+
+                ((UpDownBase)MinBtn).Text = "0";
+
+                MinBtn.Enabled = true;
+                ResetBtn.Enabled = true;
+
+                OkBtn.Enabled = true;
+                OkBtn.Visible = true;
+            }
+        }
+
+        private void OnPaintUpdate(object sender, EventArgs e)
+        {
+            canDraw = true;
+
+            min++;
+
+            Invalidate();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (isStarted)
+            {
+                min = 0;
+
+                canDraw = false;
+
+                isStarted = false;
+                StartBtn.Enabled = false;
+
+                ResetBtn.Enabled = true;
+
+                if (timer != null)
+                {
+                    timer.Stop();
+                }
+            }
+            else
+            {
+                isStarted = true;
+                StartBtn.Text = "STOP";
+                StartBtn.Enabled = true;
+
+                ResetBtn.Enabled = false;
+
+                if (timer != null)
+                {
+                    timer.Start();
+                }
+            }
+        }
+
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+            min = 0;
+
+            canDraw = true;
+
+            Invalidate();
+
+            ((UpDownBase)MinBtn).Text = "0";
+
+            MinBtn.Enabled = true;
+
+            StartBtn.Text = "START";
+
+            StartBtn.Enabled = false;
+            StartBtn.Visible = false;
+
+            OkBtn.Enabled = true;
+            OkBtn.Visible = true;
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            if (timer != null)
+            {
+                timer.Stop();
+                timer = null;
+            }
+
+            this.Dispose();
+
+            Application.Exit();
+        }
+
+        private void OkBtn_Click(object sender, EventArgs e)
+        {
+            int val = Convert.ToInt32(((UpDownBase)MinBtn).Text);
+            if (val < 1 || val > 60)
+            {
+                CMessageBox.Show(this, "Please, Input 1 ~ 60 Minutes");
+                return;
+            }
+
+            min = 60 - val;
+
+            canDraw = true;
+
+            Invalidate();
+
+            MinBtn.Enabled = false;
+
+            OkBtn.Enabled = false;
+            OkBtn.Visible = false;
+
+            StartBtn.Enabled = true;
+            StartBtn.Visible = true;
+        }
+    }
+}
